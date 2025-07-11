175dba386bb02abd19b5d596b69b8207424178d6
Bong Zak Heng <zakheng00@gmail.com>
2025-07-11 09:52:28 +0800

    Initial commit with app and requirements

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Colab API 端點 (替換為實際 NGROK URL)
-COLAB_URL = "https://d7eed30228fc.ngrok-free.app/predict_colab"  # 根據最新 Colab URL 更新
+COLAB_URL = "https://fe42908582a4.ngrok-free.app/predict_colab"  # 根據最新 Colab URL 更新
 
 # --- 路由 ---
 @app.route('/')
