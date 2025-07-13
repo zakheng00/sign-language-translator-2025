c6f256f344ff259e7e01d18c720fbbc3785e536e
user <107787753+zakheng00@users.noreply.github.com>
2025-07-14 02:25:10 +0800

    Update app.py

--- app.py
+++ app.py
@@ -31,8 +31,8 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Colab API 端點 (替換為實際 NGROK URL)
-COLAB_URL = "https://67387521b9d9.ngrok-free.app/predict_colab"  # 根據最新 Colab URL 更新
-COLAB_STT_URL = "https://67387521b9d9.ngrok-free.app/speech_to_text"  # 根據最新 Colab URL 更新
+COLAB_URL = "https://6d0f12261ab0.ngrok-free.app/predict_colab"  # 根據最新 Colab URL 更新
+COLAB_STT_URL = "https://6d0f12261ab0.ngrok-free.app/speech_to_text"  # 根據最新 Colab URL 更新
 
 # --- MongoDB 設置 ---
 def get_db():
