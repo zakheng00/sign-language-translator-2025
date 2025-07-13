0d622ca36262e74def866cdc14523a38c0ffe4bd
user <107787753+zakheng00@users.noreply.github.com>
2025-07-14 00:59:29 +0800

    Update app.py

--- app.py
+++ app.py
@@ -30,8 +30,8 @@ logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
 # Colab API 端點 (替換為實際 NGROK URL)
-COLAB_URL = "https://47e31c67464a.ngrok-free.app/predict_colab"  # 根據最新 Colab URL 更新
-COLAB_STT_URL = "https://47e31c67464a.ngrok-free.app/speech_to_text"  # 根據最新 Colab URL 更新
+COLAB_URL = "https://262a79671c64.ngrok-free.app/predict_colab"  # 根據最新 Colab URL 更新
+COLAB_STT_URL = "https://262a79671c64.ngrok-free.app/speech_to_text"  # 根據最新 Colab URL 更新
 
 # --- 路由 ---
 @app.route('/')
