b5ec8912608d26f0159f4d03e6a5895b065ee260
user <107787753+zakheng00@users.noreply.github.com>
2025-07-18 23:10:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ logging.basicConfig(
 logger.setLevel(logging.INFO)
 
 # Colab API 端點
-COLAB_BASE_URL = os.environ.get('COLAB_BASE_URL', "https://54ffeada27a5.ngrok-free.app")  # 使用 Colab 的 ngrok URL
+COLAB_BASE_URL = os.environ.get('COLAB_BASE_URL', "https://7d1d03ca5ce8.ngrok-free.app")  # 使用 Colab 的 ngrok URL
 COLAB_PREDICT_URL = f"{COLAB_BASE_URL}/predict_colab"
 COLAB_STT_URL = f"{COLAB_BASE_URL}/speech_to_text"
 COLAB_HEALTH_URL = f"{COLAB_BASE_URL}/health"
