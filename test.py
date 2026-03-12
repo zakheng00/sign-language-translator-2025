56ba85d53cdc1d67424a49957989e9cd0f5b43ff
user <107787753+zakheng00@users.noreply.github.com>
2026-03-12 12:43:15 +0800

    Delete test.py

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import requests
-     
-X_train = np.load('X_train.npy')
-frames = X_train[0].tolist()  # 取第一個樣本
-response = requests.post('http://localhost:5000/predict', json={'frames': frames})
-print(response.json())
